6c4c4b6d078161fa22029bf05f1f3d9dfcad1b00
Boru Isako <isco.jaldy@gmail.com>
2025-05-31 16:48:15 +0300

    Completed labs

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-    def __init__(self, discount=0):
-        self.discount = discount
-        self.total = 0
-        self.items = []
-        self.last_transaction_amount = 0
+  def __init__(self, discount=0, total=0, items=None, last_transaction_amount=0):
+    self.discount = discount
+    self.total = total
+    self.items = items if items is not None else []
+    self.last_transaction_amount = last_transaction_amount
+
     
   def add_item(self, title, price, quantity=1):
     self.total += price * quantity
